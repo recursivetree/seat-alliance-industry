f966f10f74f1cbc25628422ebaf7b0cfe7127261
recursive_tree <recursivetreemail@gmail.com>
2022-12-28 21:03:25 +0100

    update api to treelib items

--- src/Api/AllianceIndustryApi.php
+++ src/Api/AllianceIndustryApi.php
@@ -3,7 +3,6 @@
 namespace RecursiveTree\Seat\AllianceIndustry\Api;
 
 use RecursiveTree\Seat\AllianceIndustry\Helpers\SettingHelper;
-use Seat\Eveapi\Models\Sde\InvType;
 use Seat\Eveapi\Models\Universe\UniverseStation;
 use Seat\Eveapi\Models\Universe\UniverseStructure;
 
@@ -12,17 +11,7 @@ class AllianceIndustryApi
     public static function create_orders($data){
         $location_id = $data["location"] ?? 60003760;
 
-
-
-        $multibuy = "";
-        foreach ($data["items"] as $item){
-            $type = InvType::find($item["type_id"]);
-
-            if($type === null) continue;
-
-            $quantity = $item["quantity"] ?? $item["amount"];
-            $multibuy .= "$type->typeName $quantity" . PHP_EOL;
-        }
+        $multibuy = $data["items"]->getMultibuy();
 
         $stations = UniverseStation::all();
         $structures = UniverseStructure::all();
